9c3b0325cd8940067f85a650866f64eb9e747d9a
Champak's World <champaksworld@gmail.com>
2026-03-25 14:39:07 +0530

    Experimented a  bit

--- src/main/java/live/learnwithchampak/demo/controller/PageController.java
+++ src/main/java/live/learnwithchampak/demo/controller/PageController.java
@@ -1,4 +1,5 @@
 package live.learnwithchampak.demo.controller;
+
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,14 +7,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
- @GetMapping("/about")
-    public String about(@RequestParam(defaultValue = "Champak") String name,Model model) {
-        model.addAttribute("name",name);
+    @GetMapping("/testing")
+    public String testing(Model model) {
+        model.addAttribute("pageTitle", "Thymeleaf Home");
+        model.addAttribute("message", "This is my Message.");
+        model.addAttribute("name", "Champak");
+        model.addAttribute("topics", java.util.List.of("Spring", "Thymeleaf", "MVC"));
+        return "test";
+    }
+
+    @GetMapping("/about")
+    public String about(@RequestParam(defaultValue = "Champak") String name, Model model) {
+        model.addAttribute("name", name);
         return "about";
     }
+
     @GetMapping("/me")
     public String home(Model model) {
-           model.addAttribute("name", "Champak");
+        model.addAttribute("name", "Champak");
         return "index";
     }
 }
