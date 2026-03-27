52424ff420c2f75b7dfa936f88686bd330fbe22d
Champak's World <champaksworld@gmail.com>
2026-03-27 14:34:14 +0530

    Checked get & Post Methods

--- src/main/java/live/learnwithchampak/demo/controller/HelloController.java
+++ src/main/java/live/learnwithchampak/demo/controller/HelloController.java
@@ -1,17 +1,28 @@
 package live.learnwithchampak.demo.controller;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @PostMapping("/postmap")
+    public String posttwa() {
+        return "Post Wa";
+    }
+
+    @GetMapping("/getmap")
+    public String getwa() {
+        return "Get Wa";
+    }
 
     @GetMapping("/")
     public String home() {
         return "Spring Starter  is running.";
     }
- 
+
     @GetMapping("/ping")
     public String ping() {
         return "pong";
--- src/main/java/live/learnwithchampak/demo/controller/PageController.java
+++ src/main/java/live/learnwithchampak/demo/controller/PageController.java
@@ -1,9 +1,13 @@
 package live.learnwithchampak.demo.controller;
 
 import org.springframework.ui.Model;
+import org.apache.catalina.connector.Request;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PageController {
@@ -26,5 +30,12 @@ public class PageController {
     public String home(Model model) {
         model.addAttribute("name", "Champak");
         return "index";
+    }
+
+    @RequestMapping(value = "/req", method = RequestMethod.GET)
+    public String req(@RequestParam(defaultValue = "Jetha") String name, Model model) {
+        System.out.println(name);
+        model.addAttribute("name", name);
+        return "req";
     }
 }
